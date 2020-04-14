cde89ed993b550b1eef4ef5a63a9f308320d4d1f
Bharathi <baroselva@gmail.com>
2020-04-14 07:54:42 +0530

    commit

--- app.js
+++ app.js
@@ -111,10 +111,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000 || process.env.PORT, function() {
-  console.log("Server started on port 3000");
-
-});
+app.listen(process.env.PORT);
 
 
 
